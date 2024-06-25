dfeb8fb90dd86205ca861e76b58e3dbe87cbdca3
dsabljic <domagoj.sabljicc@gmail.com>
2024-06-25 10:42:23 +0200

    presentation and service layer for user auth

---
+++ src/main/java/com/inventory/backend/controllers/UserController.java
@@ -0,0 +1,68 @@
+package com.inventory.backend.controllers;
+
+import com.inventory.backend.dto.UserDto;
+import com.inventory.backend.entities.User;
+import com.inventory.backend.services.UserManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserManagementService userManagementService;
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<UserDto> register(@RequestBody UserDto reg){
+        return ResponseEntity.ok(userManagementService.register(reg));
+    }
+
+    // user creation alternative with custom status code
+//    @PostMapping("/auth/register")
+//    public ResponseEntity<UserDto> register(@RequestBody UserDto reg){
+//        UserDto response = userManagementService.register(reg);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+//    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<UserDto> login(@RequestBody UserDto req){
+        return ResponseEntity.ok(userManagementService.login(req));
+    }
+
+    @PostMapping("/auth/refresh")
+    public ResponseEntity<UserDto> refreshToken(@RequestBody UserDto req){
+        return ResponseEntity.ok(userManagementService.refreshToken(req));
+    }
+
+    @GetMapping("/admin/get-all-users")
+    public ResponseEntity<UserDto> getAllUsers(){
+        return ResponseEntity.ok(userManagementService.getAllUsers());
+
+    }
+
+    @GetMapping("/admin/get-users/{userId}")
+    public ResponseEntity<UserDto> getUSerByID(@PathVariable Long userId){
+        return ResponseEntity.ok(userManagementService.getUsersById(userId));
+    }
+
+    @PutMapping("/admin/update/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId, @RequestBody User reqres){
+        return ResponseEntity.ok(userManagementService.updateUser(userId, reqres));
+    }
+
+    @GetMapping("/adminuser/get-profile")
+    public ResponseEntity<UserDto> getMyProfile(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        UserDto response = userManagementService.getMyInfo(email);
+        return  ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @DeleteMapping("/admin/delete/{userId}")
+    public ResponseEntity<UserDto> deleteUSer(@PathVariable Long userId){
+        return ResponseEntity.ok(userManagementService.deleteUser(userId));
+    }
+}
---
+++ src/main/java/com/inventory/backend/services/UserManagementService.java
@@ -0,0 +1,201 @@
+package com.inventory.backend.services;
+
+import com.inventory.backend.entities.User;
+import com.inventory.backend.repositories.UserRepo;
+import com.inventory.backend.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserManagementService {
+
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private JWTUtils jwtUtils;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public UserDto register(UserDto registrationRequest){
+        UserDto resp = new UserDto();
+
+        try {
+            User user = new User();
+            user.setEmail(registrationRequest.getEmail());
+            user.setRole(registrationRequest.getRole());
+            user.setName(registrationRequest.getName());
+            user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+            User ourUsersResult = userRepo.save(user);
+            if (ourUsersResult.getId()>0) {
+                resp.setUser((ourUsersResult));
+                resp.setMessage("User Saved Successfully");
+                resp.setStatusCode(200);
+            }
+
+        } catch (Exception e){
+            resp.setStatusCode(500);
+            resp.setError(e.getMessage());
+        }
+        return resp;
+    }
+
+    public UserDto login(UserDto loginRequest){
+        UserDto response = new UserDto();
+        try {
+            authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),
+                            loginRequest.getPassword()));
+            var user = userRepo.findByEmail(loginRequest.getEmail()).orElseThrow();
+            var jwt = jwtUtils.generateToken(user);
+            var refreshToken = jwtUtils.generateRefreshToken(new HashMap<>(), user);
+            response.setStatusCode(200);
+            response.setToken(jwt);
+            response.setRole(user.getRole());
+            response.setRefreshToken(refreshToken);
+            response.setExpirationTime("24Hrs");
+            response.setMessage("Successfully Logged In");
+
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage(e.getMessage());
+        }
+        return response;
+    }
+
+    public UserDto refreshToken(UserDto refreshTokenRequest){
+        UserDto response = new UserDto();
+        try{
+            String ourEmail = jwtUtils.extractUsername(refreshTokenRequest.getToken());
+            User users = userRepo.findByEmail(ourEmail).orElseThrow();
+            if (jwtUtils.isTokenValid(refreshTokenRequest.getToken(), users)) {
+                var jwt = jwtUtils.generateToken(users);
+                response.setStatusCode(200);
+                response.setToken(jwt);
+                response.setRefreshToken(refreshTokenRequest.getToken());
+                response.setExpirationTime("24Hr");
+                response.setMessage("Successfully Refreshed Token");
+            }
+            response.setStatusCode(200);
+            return response;
+
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage(e.getMessage());
+            return response;
+        }
+    }
+
+    public UserDto getAllUsers() {
+        UserDto reqRes = new UserDto();
+
+        try {
+            List<User> result = userRepo.findAll();
+            if (!result.isEmpty()) {
+                reqRes.setUsers(result);
+                reqRes.setStatusCode(200);
+                reqRes.setMessage("Successful");
+            } else {
+                reqRes.setStatusCode(404);
+                reqRes.setMessage("No users found");
+            }
+            return reqRes;
+        } catch (Exception e) {
+            reqRes.setStatusCode(500);
+            reqRes.setMessage("Error occurred: " + e.getMessage());
+            return reqRes;
+        }
+    }
+
+    public UserDto getUsersById(Long id) {
+        UserDto reqRes = new UserDto();
+        try {
+            User usersById = userRepo.findById(id).orElseThrow(() -> new RuntimeException("User Not found"));
+            reqRes.setUser(usersById);
+            reqRes.setStatusCode(200);
+            reqRes.setMessage("Users with id '" + id + "' found successfully");
+        } catch (Exception e) {
+            reqRes.setStatusCode(500);
+            reqRes.setMessage("Error occurred: " + e.getMessage());
+        }
+        return reqRes;
+    }
+
+    public UserDto deleteUser(Long userId) {
+        UserDto reqRes = new UserDto();
+        try {
+            Optional<User> userOptional = userRepo.findById(userId);
+            if (userOptional.isPresent()) {
+                userRepo.deleteById(userId);
+                reqRes.setStatusCode(200);
+                reqRes.setMessage("User deleted successfully");
+            } else {
+                reqRes.setStatusCode(404);
+                reqRes.setMessage("User not found for deletion");
+            }
+        } catch (Exception e) {
+            reqRes.setStatusCode(500);
+            reqRes.setMessage("Error occurred while deleting user: " + e.getMessage());
+        }
+        return reqRes;
+    }
+
+    public UserDto updateUser(Long userId, User updatedUser) {
+        UserDto reqRes = new UserDto();
+        try {
+            Optional<User> userOptional = userRepo.findById(userId);
+            if (userOptional.isPresent()) {
+                User existingUser = userOptional.get();
+                existingUser.setEmail(updatedUser.getEmail());
+                existingUser.setName(updatedUser.getName());
+                existingUser.setRole(updatedUser.getRole());
+
+                // checking for password presence
+                if (updatedUser.getPassword() != null && !updatedUser.getPassword().isEmpty()) {
+                    existingUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+                }
+
+                User savedUser = userRepo.save(existingUser);
+                reqRes.setUser(savedUser);
+                reqRes.setStatusCode(200);
+                reqRes.setMessage("User updated successfully");
+            } else {
+                reqRes.setStatusCode(404);
+                reqRes.setMessage("User not found for update");
+            }
+        } catch (Exception e) {
+            reqRes.setStatusCode(500);
+            reqRes.setMessage("Error occurred while updating user: " + e.getMessage());
+        }
+        return reqRes;
+    }
+
+    public UserDto getMyInfo(String email){
+        UserDto reqRes = new UserDto();
+        try {
+            Optional<User> userOptional = userRepo.findByEmail(email);
+            if (userOptional.isPresent()) {
+                reqRes.setUser(userOptional.get());
+                reqRes.setStatusCode(200);
+                reqRes.setMessage("successful");
+            } else {
+                reqRes.setStatusCode(404);
+                reqRes.setMessage("User not found for update");
+            }
+
+        }catch (Exception e){
+            reqRes.setStatusCode(500);
+            reqRes.setMessage("Error occurred while getting user info: " + e.getMessage());
+        }
+        return reqRes;
+    }
+}
