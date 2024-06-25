a927e033a23df8338b80c22006709a8d52969ceb
dsabljic <domagoj.sabljicc@gmail.com>
2024-06-25 11:37:56 +0200

    loading in env variables

--- src/main/java/com/inventory/backend/BackendApplication.java
+++ src/main/java/com/inventory/backend/BackendApplication.java
@@ -2,11 +2,17 @@ package com.inventory.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("KEYSTORE_PASSWORD", dotenv.get("KEYSTORE_PASSWORD"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
