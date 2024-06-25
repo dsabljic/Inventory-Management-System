20f17e3a169db2b3cb906c85d39ea6d708103288
dsabljic <domagoj.sabljicc@gmail.com>
2024-06-25 10:01:30 +0200

    user service and dto

---
+++ src/main/java/com/inventory/backend/dto/UserDto.java
@@ -0,0 +1,27 @@
+package com.inventory.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.inventory.backend.entities.User;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDto {
+
+    private int statusCode;
+    private String error;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private String name;
+    private String role;
+    private String email;
+    private String password;
+    private User user;
+    private List<User> users;
+}
---
+++ src/main/java/com/inventory/backend/services/UserService.java
@@ -0,0 +1,21 @@
+package com.inventory.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.inventory.backend.repositories.UserRepo;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepo.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
